e663331e231ffdf534e86d3f06efc338fa58e01c
Rudra006 <rudramadhab006@gmail.com>
2023-10-02 20:19:44 +0530

    initial author routs

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ if(process.env.NODE_ENV !=='production'){
 const express= require("express");
 const expressLayouts = require("express-ejs-layouts");
 
-const app=express();   
+const app=express();  
+
+const bodyParser=require("body-parser")
 
 const indexRouter=require("./routs/index")
+const authorRouter=require("./routs/authors")
 app.set("view engine","ejs");
 
 app.set('views',__dirname +'/views')
@@ -17,6 +20,7 @@ app.set('layout','layouts/layout')
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit:'10mb',extended: false}))
 
 const mongoose=require("mongoose")
 
@@ -29,5 +33,6 @@ db.once('open',()=>console.log('connected to mongoose'))
 
 
 app.use('/',indexRouter);
+app.use('/authors',authorRouter);
 
 app.listen(process.env.PORT || 3000)
